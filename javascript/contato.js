417214979990499f173b6bf6b6082ca89bedaf49
pedrojsantos16-ux <pedro.j.santos16@aluno.senai.br>
2026-02-13 13:58:45 -0300

    correção

--- javascript/contato.js
+++ javascript/contato.js
@@ -1,1 +1,18 @@
 console.log("Página de contato carregada com sucesso!");
+const elementosAnimar = document.querySelectorAll('.animar');
+
+function animarAoRolar() {
+    const alturaTela = window.innerHeight;
+
+    elementosAnimar.forEach(elemento => {
+        const posicao = elemento.getBoundingClientRect().top;
+
+        if (posicao < alturaTela - 100) {
+            elemento.classList.add('ativo');
+        }
+    });
+}
+
+window.addEventListener('scroll', animarAoRolar);
+window.addEventListener('load', animarAoRolar);
+
