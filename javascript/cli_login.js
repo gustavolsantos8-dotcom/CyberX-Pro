022d4e688509cac08bc172eb71082478591ae24f
CyberX Pro <gustavo.l.santos8@aluno.senai.br>
2026-02-25 16:44:11 -0300

    atualização 25/02

---
+++ javascript/cli_login.js
@@ -0,0 +1,22 @@
+function logar(){
+    const emailDigitado = document.getElementById("email").value;
+    const senhaDigitada = document.getElementById("senha").value;
+    const mensagem = document.getElementById("mensagem");
+
+    const usuario = JSON.parse(localStorage.getItem("usuario"));
+
+ 
+
+    if(emailDigitado === usuario.email && senhaDigitada === usuario.senha){
+        mensagem.style.color = "green";
+        mensagem.innerHTML = "Login realizado com sucesso!";
+
+        setTimeout(()=>{
+            window.location.href = "index.html"; 
+        },1000);
+    }
+    else{
+        mensagem.style.color = "red";
+        mensagem.innerHTML = "Email ou senha incorretos";
+    }
+}
