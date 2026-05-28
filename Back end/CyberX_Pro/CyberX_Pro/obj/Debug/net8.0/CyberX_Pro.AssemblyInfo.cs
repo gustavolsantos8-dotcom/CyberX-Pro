0d4fe809e4a9531b8caa9cc1caa6eaff4ed188c4
Gustavoluis <sgustavoluis1@gmail.com>
2026-05-28 14:46:02 -0300

    adição do banco e mudança no script
    adição no atributo data de funcionarios

--- Back end/CyberX_Pro/CyberX_Pro/obj/Debug/net8.0/CyberX_Pro.AssemblyInfo.cs
+++ Back end/CyberX_Pro/CyberX_Pro/obj/Debug/net8.0/CyberX_Pro.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CyberX_Pro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95862d05360474eb3384e4f4a2fcc6483a6a9eef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28072e27a3560741e0958701885e28e1a9e02c38")]
 [assembly: System.Reflection.AssemblyProductAttribute("CyberX_Pro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CyberX_Pro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
