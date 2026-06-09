70a54345207bfb6a0b7a0f51f6d7a01aeaa7e640
pedrojsantos16-ux <pedro.j.santos16@aluno.senai.br>
2026-06-09 17:02:11 -0300

    atualização

--- Back end/CyberX_Pro/CyberX_Pro/obj/Debug/net8.0/CyberX_Pro.AssemblyInfo.cs
+++ Back end/CyberX_Pro/CyberX_Pro/obj/Debug/net8.0/CyberX_Pro.AssemblyInfo.cs
@@ -14,7 +14,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CyberX_Pro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< Updated upstream
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3709a6d413b43ffb862d3369c4f87d0383825b7")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22a57603e792aa2d9c57a0fea418ca058bba7ad3")]
+>>>>>>> Stashed changes
 [assembly: System.Reflection.AssemblyProductAttribute("CyberX_Pro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CyberX_Pro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
